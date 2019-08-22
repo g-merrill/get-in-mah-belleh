39601f3257965719b5de5326961be107b3b7bf56
Greg <gmerrill03@gmail.com>
2019-08-21 22:41:42 -0700

    get users pagelogin and logout working

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 const usersCtrl = require('../controllers/users');
 
 
 /* GET users listing. */
 router.get('/users', usersCtrl.index);
+
+// OAuth logout route that redirects to current page
+router.get('/users/logout', (req, res) => {
+  req.logout();
+  res.redirect('/users');
+});
 
 module.exports = router;
