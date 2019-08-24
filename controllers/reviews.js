c84e0d4346adec7392c051e50a0be0269d0044cb
Greg <gmerrill03@gmail.com>
2019-08-24 15:52:54 -0700

    save review to user and truck arrays

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,5 +1,6 @@
 const Review = require('../models/review');
 const Truck = require('../models/truck');
+const User = require('../models/user');
 
 module.exports = {
     new: newReview,
@@ -26,9 +27,28 @@ function create(req, res) {
     Review.create(req.body)
     .then(review => review.save())
     .then(review => {
-        // res.redirect(`/trucks/${req.body.truck}`);
         console.log(review);
-        res.redirect(`/trucks/${review.truck}`);
+        User.findById(req.user.id)
+        .then(user => {
+            user.reviews.push(review.id);
+            return user.save();
+        })
+        .then(user => {
+            console.log(user);
+            return Truck.findById(review.truck);
+        })
+        .then(truck => {
+            truck.reviews.push(review.id);
+            return truck.save();
+        })
+        .then(truck => {
+            console.log(truck);
+            res.redirect(`/trucks/${truck.id}`);
+        })
+        .catch(err => {
+            if (err) console.log(err);
+            res.redirect(`/trucks/${req.body.truck}`);
+        });
     })
     .catch(err => {
         if (err) console.log(err);
