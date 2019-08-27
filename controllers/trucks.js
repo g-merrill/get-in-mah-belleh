c841efe7ba9497cc17b87f190a509d1b7473addb
Greg <gmerrill03@gmail.com>
2019-08-27 00:37:44 -0700

    add styling, make background colors uniform

--- controllers/trucks.js
+++ controllers/trucks.js
@@ -13,7 +13,9 @@ module.exports = {
 
 function index(req, res) {
     Truck.find({})
+    .populate('reviews')
     .then(trucks => {
+        console.log(trucks);
         res.render('trucks/index', {
             user: req.user,
             viewName: 'trucks-index',
